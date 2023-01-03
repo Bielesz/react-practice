5082088f22a06769894ecc5e1339969512980aac
Mariusz Bielesz <bielesz@yahoo.com>
2023-01-03 14:32:11 +0100

    practice react repo

---
+++ search/src/components/ImageShow.js
@@ -0,0 +1,10 @@
+function ImageShow({ image }) {
+  return (
+    <div>
+      <img src={image.urls.small} alt={image.alt_description} />
+      <p>{image.alt_description}</p>
+    </div>
+  );
+}
+
+export default ImageShow;
