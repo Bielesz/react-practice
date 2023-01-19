7ccfab80a7c544f168e1ce4c792968d37224d786
Mariusz Bielesz <bielesz@yahoo.com>
2023-01-19 18:12:53 +0100

    Adding automatic data refetching

--- media/src/store/apis/albumsApi.js
+++ media/src/store/apis/albumsApi.js
@@ -1,5 +1,5 @@
-import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
-import { faker } from '@faker-js/faker';
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { faker } from "@faker-js/faker";
 
 // DEV ONLY!!!
 const pause = (duration) => {
@@ -9,25 +9,36 @@ const pause = (duration) => {
 };
 
 const albumsApi = createApi({
-  reducerPath: 'albums',
+  reducerPath: "albums",
   baseQuery: fetchBaseQuery({
-    baseUrl: 'http://localhost:3005',
+    baseUrl: "http://localhost:3005",
     fetchFn: async (...args) => {
       // REMOVE FOR PRODUCTION
-      await pause(1000);
+      // await pause(1000);
       return fetch(...args);
     },
   }),
   endpoints(builder) {
     return {
+      removeAlbum: builder.mutation({
+        invalidatesTags: (result, error, album) => {
+          return [{ type: "Album", id: album.id }];
+        },
+        query: (album) => {
+          return {
+            url: `/albums/${album.id}`,
+            method: "DELETE",
+          };
+        },
+      }),
       addAlbum: builder.mutation({
         invalidatesTags: (result, error, user) => {
-          return [{ type: 'Album', id: user.id }];
+          return [{ type: "UsersAlbums", id: user.id }];
         },
         query: (user) => {
           return {
-            url: '/albums',
-            method: 'POST',
+            url: "/albums",
+            method: "POST",
             body: {
               userId: user.id,
               title: faker.commerce.productName(),
@@ -37,15 +48,19 @@ const albumsApi = createApi({
       }),
       fetchAlbums: builder.query({
         providesTags: (result, error, user) => {
-          return [{ type: 'Album', id: user.id }];
+          const tags = result.map((album) => {
+            return { type: "Album", id: album.id };
+          });
+          tags.push({ type: "UsersAlbums", id: user.id });
+          return tags;
         },
         query: (user) => {
           return {
-            url: '/albums',
+            url: "/albums",
             params: {
               userId: user.id,
             },
-            method: 'GET',
+            method: "GET",
           };
         },
       }),
@@ -53,5 +68,9 @@ const albumsApi = createApi({
   },
 });
 
-export const { useFetchAlbumsQuery, useAddAlbumMutation } = albumsApi;
+export const {
+  useFetchAlbumsQuery,
+  useAddAlbumMutation,
+  useRemoveAlbumMutation,
+} = albumsApi;
 export { albumsApi };
